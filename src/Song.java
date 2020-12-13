d0fbaed168e82d51f7146a5f54409d24c55fd247
Sevda ERGÜN <sevda_ergun@eskisehir.edu.tr>
2020-12-13 14:57:01 +0300

    Last changes

---
+++ src/Song.java
@@ -0,0 +1,15 @@
+public class Song {
+    private String songName;
+    Song next;
+
+    public Song(String songName) {
+        this.songName = songName;
+        this.next = null;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+
+}
